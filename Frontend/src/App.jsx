55d173c689ce3e320f455633854a78277f7f19b0
Talha <talhatanveer202@gmail.com>
2025-11-06 00:17:58 +0500

    Add new changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
-import AuthLayout from './components/ui/auth/layout'
+import AuthLayout from './components/auth/layout'
 import AuthRegister from './pages/auth/register'
 import AuthLogin from './pages/auth/login'
+import AdminLayout from './components/admin-view/layout'
+import AdminDashboard from './pages/admin-view/dashboard'
+import AdminProducts from './pages/admin-view/products'
+import AdminOrders from './pages/admin-view/orders'
+import AdminFeatures from './pages/admin-view/features'
 
 function App() {
   return (
@@ -11,6 +16,12 @@ function App() {
         <Route path='/auth' element={<AuthLayout/>}>
         <Route path='login' element={<AuthLogin/>}/>
         <Route path='register' element={<AuthRegister/>}/>
+        </Route>
+        <Route path='/admin' element={<AdminLayout/>}>
+        <Route path='dashboard' element={<AdminDashboard/>}/>
+        <Route path='products' element={<AdminProducts/>}/>
+        <Route path='orders' element={<AdminOrders/>}/>
+        <Route path='features' element={<AdminFeatures/>}/>
         </Route>
       </Routes>
 
---
+++ Frontend/src/components/shopping-view/layout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ShoppingLayout() {
+  return (
+    <div>layout</div>
+  )
+}
+
+export default ShoppingLayout
